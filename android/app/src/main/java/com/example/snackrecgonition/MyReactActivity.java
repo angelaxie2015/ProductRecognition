d3ea7b0acba09f2409b78b47f3d3a73664483ae3
Raymond <wrg7yu@gmail.com>
2022-03-29 14:59:15 -0400

    feat: Integrate React Native with Android App

---
+++ android/app/src/main/java/com/example/snackrecgonition/MyReactActivity.java
@@ -0,0 +1,14 @@
+package com.example.snackrecgonition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyReactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_react);
+    }
+}
